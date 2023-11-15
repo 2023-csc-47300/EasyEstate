ce4ab4f2b5070b5bd46d0b7e7863abc42a740ff3
programmerjoban <jobanpreet.inbox@gmail.com>
2023-11-14 23:11:16 -0500

    created new react project using create-react-app

--- src/App.js
+++ src/App.js
@@ -4,20 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+       
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is the Main Page for Easy Estate. <br></br>
+          <br></br>
+          Edit this in APP.js <br></br>or take out render /APP in index.js page and write code there
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
